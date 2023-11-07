1ad592349218654a5d36e366e461976055f34a7e
Vivian De Mitry <vivian090@gmail.com>
2023-11-07 12:54:54 -0300

    first commit

---
+++ aula1/operadores_matematicos.py
@@ -0,0 +1,19 @@
+# Soma
+print('1 + 1 =', 1+1)
+
+# Subtração
+print('2 - 1 =', 2-1)
+
+# Multiplicação
+print('3 x 1 =', 3*1)
+
+# Divisão
+print('3 / 3 =', 3/3)
+
+# Módulo
+print('3 / 3 =', 3%3)
+
+# Exponencial
+print('3 ao cubo e igual a', 3**3)
+
+
