978daf6c43799ca9ad65060bea1969be48bffce8
Vivian De Mitry <vivian090@gmail.com>
2023-11-07 13:22:47 -0300

    cadastro

---
+++ aula1/cadastro.py
@@ -0,0 +1,7 @@
+# Variaveis
+
+print('Cadastro de veiculos\n')
+nome_do_veiculo = input('Digite o nome do veiculo que deseja cadastrar: ')
+ano_do_veiculo = input('Digite o ano deste veiculo: ')
+
+print(f'{nome_do_veiculo}|{ano_do_veiculo} foi cadastrado com sucesso!')
