f409be973e17a5a43986844e623e0d34fd09e818
Vivian De Mitry <vivian090@gmail.com>
2023-11-07 13:09:30 -0300

    operadores e tipos de dados

---
+++ aula1/comparacao.py
@@ -0,0 +1,17 @@
+#Compara se é igual
+print(5==5)
+
+#Difernete
+print(5 != 5)
+
+#Maior que
+print(42 > 5)
+
+#Maior ou igual que
+print(42 >= 5)
+
+#Menor que
+print(42 < 5)
+
+#Menor ou igual que
+print(42 <= 5)
---
+++ aula1/tipos_de_dados.py
@@ -0,0 +1,11 @@
+print(type('hello world')) # 'str' (string)
+
+print(type(True)) # bool
+
+print(type(False)) # bool (boolean)
+
+print(type(3)) # int (inteiro)
+
+print(type(10/3)) # float
+
+print(type({'chave':'valor', 'outra_chave':'outro_valor'})) # dict
