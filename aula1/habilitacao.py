b9996ffcbf787940277e10163215e9bf5425cd69
Vivian De Mitry <vivian090@gmail.com>
2023-11-07 13:36:40 -0300

    habilitacao

---
+++ aula1/habilitacao.py
@@ -0,0 +1,9 @@
+print('Verifica quem pode dirigir\n')
+
+idade = int(input('Digite uma idade: '))
+
+if idade >= 18:
+    print('pode dirigir')
+    print('') 
+else:
+    print('Nao pode dirigir')    
